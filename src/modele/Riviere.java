709c5eb09488156e0108e9b1abdfc794c6a07f10
WolfoS92 <rindraandrianjanahary@gmail.com>
2025-05-19 16:22:37 +0200

    Ajout de l'eau à 20 points

--- src/modele/Riviere.java
+++ src/modele/Riviere.java
@@ -4,7 +4,7 @@ import controleur.Jeu;
 
 
 public class Riviere extends Hexagone {
-    private static final int POINTDEPLACEMENT = 2;
+    private static final int POINTDEPLACEMENT = 20;
     private static final double BONUSDEFENSE = 0.1;
     public Riviere(final int x, final int y) {
         super(Jeu.RIVIERE, BONUSDEFENSE, POINTDEPLACEMENT, x, y);
